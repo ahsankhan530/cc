059a38b7bfa6a96015c3e32f523ebfb895c6fcb8
ahsankhan530 <khanahsan530@gmail.com>
2019-09-23 02:55:28 +0500

    s

---
+++ application/config/parse.php
@@ -0,0 +1,4 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+	$config['parse_app_id'] = 'WB8WtqN4Dee6fKIaJDlkwsEdYlk7fHIgi13TR8Tb';
+	$config['parse_rest_key'] = 'UkdKHGNw5XAyHsaxxqsAJEyFZ1fWvaxhceCCwPfh';
